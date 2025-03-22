700576a546ae1861f734c2288a53bc598ee5bac0
SuperRep667 <supervalt667@gmail.com>
2025-03-22 12:51:49 +0500

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import requests as re
-from bs4 import BeautifulSoup
-
-res = re.get('https://www.film.ru/compilation/100-luchshih-filmov-xxi-veka-po-versii-zhurnala-empire')
-soup = BeautifulSoup(res.text, features='lxml')
-divs = soup.find_all('div', class_='redesign_afisha_movie_main') 
-for div in divs:
-    data = [i.strip() for i in div.text.split('\n') if i != '']
-    print(data[:10])
